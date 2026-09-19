7c65ba636ce94267c0b8a9ca776164393f835d4c
abhiram1809 <53875874+abhiram1809@users.noreply.github.com>
2026-09-20 00:40:17 +0530

    Solve expected-value-discrete

---
+++ expected-value-discrete/expected-value-discrete.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def expected_value_discrete(x: list, p: list) -> float:
+    """
+    Returns the expected value as a Python float.
+    """
+    # Write code here
+    # return float(sum([num*prob for num, prob in zip(x, p)]))
+    return float(np.dot(x,p))
